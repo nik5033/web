0e46ebc93232eeed57174efcfb9e9e9a1dc47c4a
nik5033 <gudkov.nikita@bk.ru>
2020-11-13 19:47:41 +0300

    Entrypoint

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const Connect = require('./database').Connect;
+const Create = require('./database').CreateUser;
+const UserExists = require('./database').UserExists;
+const routes = require('./routes/routes');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const Cookie = require('cookie-parser');
+const session = require('express-session');
+require('./models/user');
+//const flash = require('connect-flash');
+const config = require('./config/configs')
+
+const PORT = process.env.PORT || 4444
+const app = express();
+
+app.use(express.static( __dirname + '/public'));
+app.set('view engine', 'pug');
+
+app.use(session({
+    secret: config.secretKey,
+    resave: true,
+    secure: false,
+    saveUninitialized: true,
+    maxAge: 1000 * 3600
+}))
+//app.use(flash());
+app.use(Cookie());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended:  true }))
+app.use('/', routes);
+
+app.listen(PORT, () =>{
+    Connect()
+        .then(() =>{
+            console.log('Connected');
+            UserExists(config.admin_username)
+                .then(ans => {
+                    if(ans){
+                        console.log("Admin has been already created.");
+                    }
+                    else{
+                        Create(config.admin_username,config.admin_password, config.admin)
+                            .then(u => {
+                                console.log("Admin was created");
+                            })
+                            .catch(e => {
+                                console.log("Error: " + e);
+                            })
+                    }
+                })
+                .catch(e => {
+                    console.log("Error: " + e);
+                })
+        })
+        .catch(e => {
+            console.log('Not connected');
+        });
+});
